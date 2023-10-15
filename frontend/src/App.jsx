7ffb124d94fc5ced1d6048cd989f840fe7b7f98f
Prakhar Tripathi <tprakhar468@gmail.com>
2023-10-15 17:21:45 +0530

    Book details UI created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/books/create" element={<CreateBook />} />
-      <Route path="/books/detail/:id" element={<ShowBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
 
 const ShowBook = () => {
+
+  const [book, setBook] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5000/getBooks/${id}`)
+      .then((response) => {
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>ShowBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
+            <span>{book.publishYear}</span>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
