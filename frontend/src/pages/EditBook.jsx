c6327a5af57cdd05ddb79b2d96baf4fe2a127950
Prakhar Tripathi <tprakhar468@gmail.com>
2023-10-16 19:58:10 +0530

    Added PROD_URL in EditBook

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import { PROD_URL } from '../../constants';
 
 const EditBook = () => {
   const [title, setTitle] = useState('');
